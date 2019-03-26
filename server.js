03a3e0caf61118b4266ebd5f2b06f51aec4207bf
unknown <dreilingmarshall@gmail.com>
2019-03-26 17:49:31 -0400

    Init commit of all routes. Need testing

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ var axios = require("axios");
 // HTTP scraper
 var cheerio = require("cheerio");
 
+
 // ==================================================
 // Require models, set port, and initial Express
 // ==================================================
@@ -29,6 +30,7 @@ var PORT = 12300;
 
 // Initial Express
 var app = express();
+
 
 // =========================
 // Configure Middleware
@@ -47,11 +49,12 @@ app.use(express.static("public"));
 // Connect to Mongo DB (local)
 mongoose.connect("mongodb://localhost/scraperHW", { useNewUrlParser: true });
 
+
 // ===============
 // Set routes
 // ===============
 
-// GET route for scraping
+// GET Route: html(scrape) -> database
 app.get("/scrape", function(req, res) {
     
     // Axios pull of body of html
@@ -95,7 +98,90 @@ app.get("/scrape", function(req, res) {
                 .children("a")
                 .children("img")
                 .attr("src");
+
+            // Create a new Article using the scraped `result` object
+            db.Article.create(result)
+
+                // Promise function to show result in console
+                .then(function(dbArticle) {
+
+                    // View added result in the console
+                    console.log(dbArticle);
+                })
+
+                // Catch in case of error
+                .catch(function(err) {
+
+                    // If an error occured, log it here
+                    console.log("++ DB Article Create Error ++\n" + err)
+                });
+        });
+
+        // Send a message to the client
+        res.send("Scrape Complete!");
+    });
+});
+
+// GET Route: database -> client
+app.get("/articles", function(req, res) {
+    
+    // Grab all documents from Articles collection
+    db.Article.find({})
+
+        // Promise function to push result into client
+        .then(function(dbArticle) {
+            res.json(dbArticle);
         })
 
-    })
-})
+        // Catch function in case an error occurs. Send error to client
+        .catch(function(err) {
+            res.json(err);
+        });
+});
+
+// GET Route: Querying singular Article to show its Comments
+app.get("/articles/:id", function(req, res) {
+
+    // Prepare a query based off of the ID passed in the ID parameter
+    db.Article.findOne({ _id: req.params.id })
+
+        // Populate Article with all comments associated with it
+        .populate("comment")
+        .then(function(dbArticle) {
+
+            // If the query was successful, send it back to the client
+            res.json(dbArticle);
+        })
+        
+        // Catch function in case an error occurs. Send error to client
+        .catch(function(err) {
+            res.json(err);
+        });
+});
+
+// POST Route: Saving/Updating an Article's associated Comments
+app.post("/articles/:id", function(req, res) {
+    
+    // Create a new Comment and pass the req.body to the entry
+    db.Comment.create(req.body)
+
+        // Add Comment to the Article with the same id
+        .then(function(dbComment) {
+            return db.Article.findOneAndUpdate({ _id: req.params.id}, { comment: dbComment._id}, { new: true });
+        })
+
+        // Send updated Article with new Comment back to client
+        .then(function(dbArticle) {
+            res.json(dbArticle);
+        })
+
+        // Catch function in case an error occurs. Send error to client
+        .catch(function(err) {
+            res.json(err);
+        });
+});
+
+// Start the server
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+});
