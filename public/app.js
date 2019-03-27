4912f10faaae235ccf16ee9e5edded72dcd60a93
unknown <dreilingmarshall@gmail.com>
2019-03-27 13:40:24 -0400

    Init styling

--- public/app.js
+++ public/app.js
@@ -5,6 +5,18 @@ $.getJSON("/articles", function(data) {
     for (let i = 0; i < data.length; i++) {
 
         // Add information to client with appropriate data tags
-        $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+        console.log(data[i]);
+        $("#articles").append
+            ("<div class='entry container' data-id='" 
+            + data[i]._id 
+            + "'> <a class='link col s3' href'"
+            + data[i].link 
+            + "<img class='entry-image' src='"           
+            + data[i].image 
+            + "'></a> <div class='entry-info col s9'> <h2 class='headline'>" 
+            + data[i].headline 
+            + "</h4> <p class='summary'>" 
+            + data[i].summary
+            + "</p></div></div>");
     }
 });
